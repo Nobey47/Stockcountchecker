7e3bbd385622f58c8740ffa60caeaf3101358316
Nobey47 <110038511+Nobey47@users.noreply.github.com>
2023-02-12 14:04:58 +0000

    added arguments

--- __main__.py
+++ __main__.py
@@ -1,9 +1,25 @@
+import argparse
 import pandas as pd
-import numpy as np
-import sys
-
+import matplotlib.pyplot as plt
 
 pd.options.display.float_format = '{:.2f}'.format
+
+VAR_THRESH = 50
+COGS_THRESH = 200
+
+parser = argparse.ArgumentParser(
+    prog="StockCountChecker",
+    description="Checks stock result from EPOS automatically, finding anomalies and calculating ordering percent"
+)
+
+parser.add_argument("file")
+parser.add_argument('-s', "--sales", type=float)
+parser.add_argument('-g', "--graph", action="store_true")
+parser.add_argument('-a', "--analyze", action="store_true")
+parser.add_argument('-d', "--drop100", action="store_true")
+parser.add_argument('-o', "--output")
+args = parser.parse_args()
+
 
 class StockData:
     def __init__(self, dataframe):
@@ -46,27 +62,58 @@ class StockData:
         }
 
         self.data = raw_data.astype(datatypes)
+        self.data["Var Percent"] = self.data["Var Qty"] / self.data["Sales Qty"] * 100
+        self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"]) / \
+                                    self.data["Actual Usage Qty"] * 100
 
-    def analyze(self):
+    def analyze(self, drop100values=False):
         analysis_text = '\n'
         # missing/too much
-        self.data["Var Percent"] = self.data["Var Qty"]/self.data["Sales Qty"] * 100
-        var_outliers = self.data.loc[((self.data["Var Percent"] <= -50) & (self.data["Group"] != "Actual = Theo Usage")) | ((self.data["Var Percent"] >= 50) & (self.data["Group"] != "Actual = Theo Usage"))]
+        if drop100values:
+            var_outliers = self.data.loc[((self.data["Var Percent"] <= -VAR_THRESH) &
+                                          (self.data["Var Percent"] != -100) &
+                                          (self.data["Group"] != "Actual = Theo Usage")) |
+                                         ((self.data["Var Percent"] >= VAR_THRESH) &
+                                          (self.data["Var Percent"] != 100) &
+                                          (self.data["Group"] != "Actual = Theo Usage"))]
+        else:
+            var_outliers = self.data.loc[((self.data["Var Percent"] <= -VAR_THRESH) &
+                                          (self.data["Group"] != "Actual = Theo Usage")) |
+                                         ((self.data["Var Percent"] >= VAR_THRESH) &
+                                          (self.data["Group"] != "Actual = Theo Usage"))]
         if len(var_outliers) >= 1:
             analysis_text = analysis_text + "These numbers look off. consider recounting\n\n"
 
-            analysis_text = analysis_text + var_outliers[["Stock Description", "Case Size", "Close Qty", "Actual Usage Qty", "Sales Qty", "Var Cost", "Var Qty", "Var Percent"]].to_string(index=False) + '\n'
+            analysis_text = analysis_text + var_outliers[["Stock Description",
+                                                          "Case Size",
+                                                          "Close Qty",
+                                                          "Actual Usage Qty",
+                                                          "Sales Qty",
+                                                          "Var Cost",
+                                                          "Var Qty",
+                                                          "Var Percent"]
+            ].to_string(index=False) + '\n'
 
         analysis_text = analysis_text + "\n\n\n"
 
         # CoGs %
-        self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"])/\
-                                    self.data["Actual Usage Qty"] * 100
-        CoGs_outliers = self.data.loc[(self.data["CoGs Percent"] >= 200) | (self.data["CoGs Percent"] <= -100)]
-        if len(CoGs_outliers) >= 1:
-            analysis_text = analysis_text + "These items appear to be ordered wrong. consider ordering less next week\n\n"
+        if drop100values:
+            cogs_outliers = self.data.loc[((self.data["CoGs Percent"] >= COGS_THRESH) &
+                                           (self.data["CoGs Percent"] != 100)) |
+                                          ((self.data["CoGs Percent"] <= -COGS_THRESH) &
+                                           (self.data["CoGs Percent"] != -100))
+                                          ]
+        else:
+            cogs_outliers = self.data.loc[(self.data["CoGs Percent"] >= COGS_THRESH) |
+                                          (self.data["CoGs Percent"] <= -COGS_THRESH)
+                                          ]
+        if len(cogs_outliers) >= 1:
+            analysis_text = analysis_text + "These items appear to be ordered wrong. consider ordering less next " \
+                                            "week\n\n "
 
-            analysis_text = analysis_text + CoGs_outliers[["Stock Description", "CoGs Percent"]].to_string(index=False) + '\n'
+            analysis_text = analysis_text + cogs_outliers[["Stock Description",
+                                                           "CoGs Percent"]
+            ].to_string(index=False) + '\n'
 
         analysis_text = analysis_text + "\n\n\n"
 
@@ -75,13 +122,16 @@ class StockData:
         if len(negative_actual) >= 1:
             analysis_text = analysis_text + "The actual usage for these items is negative. Consider recounting\n\n"
 
-            analysis_text = analysis_text + negative_actual[["Stock Description", "Case Size", "Actual Usage Qty"]].to_string(index=False) + '\n'
+            analysis_text = analysis_text + negative_actual[["Stock Description",
+                                                             "Case Size",
+                                                             "Actual Usage Qty"]
+            ].to_string(index=False) + '\n'
 
         return analysis_text
 
     def save_to_csv(self, path):
-        self.data["Var Percent"] = self.data["Var Qty"]/self.data["Sales Qty"] * 100
-        self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"])/\
+        self.data["Var Percent"] = self.data["Var Qty"] / self.data["Sales Qty"] * 100
+        self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"]) / \
                                     self.data["Actual Usage Qty"] * 100
         try:
             self.data.to_csv(path)
@@ -92,14 +142,24 @@ class StockData:
             return True
 
     def UPT(self, sales):
-        self.data["UPT"] = (self.data["Actual Usage Qty"]/(sales/1000))
-        self.data[["Stock Description", "Case Size", "UPT"]].to_csv("2023-01-22_UPT.csv", index=False)
+        self.data["UPT"] = (self.data["Actual Usage Qty"] / (sales / 1000))
+
+    def make_graph(self):
+        fig, ax = plt.subplots(figsize=(15, 5))
+        ax.bar(self.data["Stock Description"], self.data["CoGs Percent"])
+        ax.set_xticklabels(SD.data["Stock Description"], rotation=90)
+        plt.show()
 
 
-if len(sys.argv) > 1:
-    try:
-        SD = StockData(pd.read_html(sys.argv[1])[0])
+try:
+    SD = StockData(pd.read_html(args.file)[0])
+    if args.analyze:
         print(SD.analyze())
-    except Exception as ex:
-        print(ex)
-
+    if args.sales:
+        SD.UPT(args.sales)
+    if args.output:
+        SD.save_to_csv(args.output)
+    if args.graph:
+        SD.make_graph()
+except Exception as ex:
+    print(ex)
