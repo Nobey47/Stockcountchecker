8d9e83561c077c405d7a0fa608c78a749cc42cb2
Nobey47 <Nobey.47@gmail.com>
2023-02-12 11:18:39 +0000

    added UPT function

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys
 
 
+pd.options.display.float_format = '{:.2f}'.format
+
 class StockData:
     def __init__(self, dataframe):
         raw_data = dataframe.dropna(axis=0, thresh=10)
@@ -49,22 +51,22 @@ class StockData:
         analysis_text = '\n'
         # missing/too much
         self.data["Var Percent"] = self.data["Var Qty"]/self.data["Sales Qty"] * 100
-        var_outliers = self.data.loc[(self.data["Var Percent"] <= -100) & (self.data["Group"] != "Actual = Theo Usage")]
+        var_outliers = self.data.loc[((self.data["Var Percent"] <= -50) & (self.data["Group"] != "Actual = Theo Usage")) | ((self.data["Var Percent"] >= 50) & (self.data["Group"] != "Actual = Theo Usage"))]
         if len(var_outliers) >= 1:
-            analysis_text = analysis_text + "Looks like we're missing a lot of these. consider recounting\n\n"
+            analysis_text = analysis_text + "These numbers look off. consider recounting\n\n"
 
-            analysis_text = analysis_text + var_outliers[["Stock Description", "Sales Qty", "Var Qty", "Var Percent"]].to_string(index=False) + '\n'
+            analysis_text = analysis_text + var_outliers[["Stock Description", "Case Size", "Close Qty", "Actual Usage Qty", "Sales Qty", "Var Cost", "Var Qty", "Var Percent"]].to_string(index=False) + '\n'
 
         analysis_text = analysis_text + "\n\n\n"
 
         # CoGs %
         self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"])/\
                                     self.data["Actual Usage Qty"] * 100
-        CoGs_outliers = self.data.loc[self.data["CoGs Percent"] >= 200]
+        CoGs_outliers = self.data.loc[(self.data["CoGs Percent"] >= 200) | (self.data["CoGs Percent"] <= -100)]
         if len(CoGs_outliers) >= 1:
-            analysis_text = analysis_text + "These items appear to be over-ordered. consider ordering less next week\n\n"
+            analysis_text = analysis_text + "These items appear to be ordered wrong. consider ordering less next week\n\n"
 
-            analysis_text = analysis_text + CoGs_outliers["Stock Description"].to_string(index=False) + '\n'
+            analysis_text = analysis_text + CoGs_outliers[["Stock Description", "CoGs Percent"]].to_string(index=False) + '\n'
 
         analysis_text = analysis_text + "\n\n\n"
 
@@ -73,11 +75,14 @@ class StockData:
         if len(negative_actual) >= 1:
             analysis_text = analysis_text + "The actual usage for these items is negative. Consider recounting\n\n"
 
-            analysis_text = analysis_text + negative_actual["Stock Description"].to_string(index=False) + '\n'
+            analysis_text = analysis_text + negative_actual[["Stock Description", "Case Size", "Actual Usage Qty"]].to_string(index=False) + '\n'
 
         return analysis_text
 
     def save_to_csv(self, path):
+        self.data["Var Percent"] = self.data["Var Qty"]/self.data["Sales Qty"] * 100
+        self.data["CoGs Percent"] = (self.data["Goods In|Out"] - self.data["Actual Usage Qty"])/\
+                                    self.data["Actual Usage Qty"] * 100
         try:
             self.data.to_csv(path)
         except Exception as ex:
@@ -85,6 +90,10 @@ class StockData:
             return False
         else:
             return True
+
+    def UPT(self, sales):
+        self.data["UPT"] = (self.data["Actual Usage Qty"]/(sales/1000))
+        self.data[["Stock Description", "Case Size", "UPT"]].to_csv("2023-01-22_UPT.csv", index=False)
 
 
 if len(sys.argv) > 1:
